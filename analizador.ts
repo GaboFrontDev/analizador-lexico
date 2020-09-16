dfe77bfc550eac578936382b0d4b26c4aa1223c3
Gabriel <gabriel.hinojoza@itexico.com>
2020-09-15 21:47:26 -0500

    NUEVOS TIPOS

--- analizador.ts
+++ analizador.ts
@@ -1,58 +1,98 @@
-const OPERATIONS_REGEX = /(\-|\+|\/|\*).?/;
-const NUMBERS_REGEX = /^(?:\d+\.?\d*|\d*\.\d+)$/;
+const IDENTIFIER = /([_a-zA-Z][_a-zA-Z0-9]{0,30})/;
+const OPERATOR_ADD = /^(\+|\-)$/;
+const OPERATOR_MULTI = /^(\*|\/)$/;
+const OPERATOR_ASIGN = /(=)$/;
+const OPERATOR_RELAT = /^(==|!=|>|<|>=|<=)$/;
+const OPERATOR_AND = /^(&&)$/;
+const OPERATOR_OR = /^(||)$/;
+const OPERATOR_NOT = /^(!)$/;
+const PARENTESIS = /(\(|\))$/;
+const BRAKETS = /^({|})$/;
+const SEMICOLON = /^(;)$/;
+const OPERATORS = /^(\+|-|\*|\/|=|==|>|<|>=|<=|&|\||!|\^)$/;
+const REAL_REGEX = /^(?:\d+\.?\d*|\d*\.\d+)$/;
 
 interface ILexico {
-  source: string;
+	source: string;
 }
 
-export const OPERATION = 0;
+export const IDENTIFIER_VAL = 0;
+export const INT = 1;
+export const REAL = 2;
+export const OPERATOR = 0;
 export const INTEGER = 1;
 export const ERROR = 2;
+export const STRING = 3;
+export const TYPE = 4;
+export const SUM = 5;
+export const MULTI = 6;
+export const RELAT = 7;
+export const OR = 8;
+export const NOT = 10;
+export const ASIGN = 18;
 const KINDS = ["OPERATION", "REAL", "NOT DEFINED"];
 
 export class Lexico {
-  private source: string[] = [];
-  private token = "";
+	private source: string[] = [];
+	private token = "";
 
-  constructor(args: ILexico) {
-    this.source = args.source.split("").reverse();
-  }
+	constructor(args: ILexico) {
+		this.source = args.source.split("").reverse();
+	}
 
-  evaluate() {
-    this.token = <string>this.source.pop();
+	evaluate() {
+		this.token = <string>this.source.pop();
+		if (this.isOperator(this.token)) {
+			return this.evaluateOperators();
+		}
 
-    this.token;
-    if (this.isOperation(this.token)) {
-      return OPERATION;
-    }
+		if (this.isReal(this.token)) {
+			return this.evaluateReal();
+		}
 
-    if (this.isReal(this.token)) {
-      while (
-        this.source.length &&
-        this.isReal(this.token + this.source[this.source.length - 1])
-      ) {
-        this.token += this.source.pop();
-      }
-      return INTEGER;
-    }
+		return ERROR;
+	}
 
-    return ERROR;
-  }
+	private isOperator(val: string) {
+		return val.match(OPERATORS);
+	}
 
-  private isOperation(val: string) {
-    return val.match(OPERATIONS_REGEX);
-  }
+	private isReal(val: string) {
+		return val.match(REAL_REGEX);
+	}
 
-  private isReal(val: string) {
-    return val.match(NUMBERS_REGEX);
-  }
+	private evaluateReal() {
+		while (
+			this.source.length &&
+			this.isReal(this.token + this.source[this.source.length - 1])
+		) {
+			this.token += this.source.pop();
+		}
+		return INTEGER;
+	}
 
-  get kind() {
-    const kind = this.evaluate();
-    return `${this.token}  ${KINDS[kind]}`;
-  }
+	private evaluateOperators() {
+		let operator_kind = 0;
 
-  get hasTokens() {
-    return this.source.length > 0;
-  }
+		const pairOperator = this.token + this.source[this.source.length - 1];
+
+		if (this.token.match(OPERATOR_ADD)) operator_kind = 2;
+		if (this.token.match(OPERATOR_MULTI)) operator_kind = 2;
+		if (this.token.match(OPERATOR_ASIGN)) operator_kind = ASIGN;
+		if (this.token.match(OPERATOR_NOT)) operator_kind = NOT;
+		if (this.token.match(OPERATOR_RELAT)) {
+			operator_kind = RELAT;
+		}
+
+		return operator_kind;
+	}
+
+	get kind() {
+		const kind = this.evaluate();
+		return `${this.token}  ${KINDS[kind]}`;
+	}
+
+	get hasTokens() {
+		return this.source.length > 0;
+	}
 }
